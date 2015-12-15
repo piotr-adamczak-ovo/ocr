8d2c0e61dff15005aadee6e1c84affd811b8c5e5
Piotr Adamczak <piotr.adamczak@ovoenergy.com>
2015-12-15 17:00:46 +0000

    Saving captured images with ocr result

--- server/routes.js
+++ server/routes.js
@@ -7,6 +7,44 @@ var textcleaner = require('textcleaner');
 var multer  = require('multer');
 var fs = require('fs');
 
+
+/**
+ * Sync file copying
+ */
+ function copyFileSync(srcFile, destFile) {
+    var BUF_LENGTH, buff, bytesRead, fdr, fdw, pos;
+    BUF_LENGTH = 64 * 1024;
+    buff = new Buffer(BUF_LENGTH);
+    fdr = fs.openSync(srcFile, 'r');
+    fdw = fs.openSync(destFile, 'w');
+    bytesRead = 1;
+    pos = 0;
+    while (bytesRead > 0) {
+        bytesRead = fs.readSync(fdr, buff, 0, BUF_LENGTH, pos);
+        fs.writeSync(fdw, buff, 0, bytesRead);
+        pos += bytesRead;
+    }
+    fs.closeSync(fdr);
+    return fs.closeSync(fdw);
+};
+
+
+function datetimestamp() {
+    var today = new Date();
+    var sToday = today.getDate().toString();
+    sToday += '-';
+    sToday += (today.getMonth()+1).toString();
+    sToday += '-';
+    sToday += today.getFullYear().toString();
+    sToday += '_';
+    sToday += today.getHours().toString();
+    sToday += '-';
+    sToday += today.getMinutes().toString();
+    sToday += '-';
+    sToday += today.getSeconds().toString();
+    return sToday;
+}
+
 module.exports = function(app) {
     app.use(multer(
         {
@@ -47,6 +85,8 @@ module.exports = function(app) {
 
                 if (!err) {
                     res.json(200, text);
+                    copyFileSync(image, __dirname + '/../uploads/'+datetimestamp()+'--'+text+'.jpg');
+ 
                 } else {
                     res.json(500, "Error while scanning image");
                 }
@@ -109,4 +149,5 @@ function performOCR(path, callback) {
             callback(null, text);
         }
     });
+
 }
