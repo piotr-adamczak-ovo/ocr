1fd0e279c888370d40596987c19ce41eac592e3d
Piotr Adamczak <piotr.adamczak@ovoenergy.com>
2015-12-16 16:34:02 +0000

    Add benchmark

--- server/routes.js
+++ server/routes.js
@@ -18,7 +18,7 @@ module.exports = function(app) {
 
     app.post("/api/ocr", process);
     app.post("/api/digits", digits);
-
+    app.get("/api/benchmark", benchmark);
 };
 
 /**
@@ -32,6 +32,54 @@ module.exports = function(app) {
  * @param req
  * @param res
  */
+
+ var benchmark = function(req, res) {
+
+    var expect = 019676;
+
+    var image = __dirname + '/../test/test_3.jpg';
+
+    //best for dark 04944 : 
+    // b_0.6_c_-0.1_p_4  49445774
+    // b_0.6_c_0.6_p_4  0494411
+    // b_0.6_c_0.5_p_4  04944152
+    // b_0.7_c_0.4_p_5_ 0494415
+
+    //best for dark 019676
+    // b_0.6_c_0.8_p_5_01967931
+
+    //best for medium 21138 : 
+    // b_0.4_c_0.8_p_5_1388
+    // b_0.4_c_0.8_p_5_1388.jpg
+    // b_0.4_c_0.85_p_15_8138
+
+
+    for (var p=5;p<=5;p+=1) {
+        for (var b=-30;b<=90;b+=10) {
+            for (var c=0; c<=100;c+=10) {
+
+            preprocessImageWithParams(image, b/100, c/100,p,function(err, preprocessedImage, br, co, po) {
+                if(!err) {
+                        performOCR(preprocessedImage, function(err, text) {
+                            if (parseInt(text) != expect) {
+                                utils.copyFileSync(preprocessedImage, __dirname + '/../uploads/meter_photo_b_'+br+'_c_'+co+'_p_'+po+'_'+text+'.jpg');
+                            } else {
+                                utils.copyFileSync(preprocessedImage, __dirname + '/../uploads/YES_meter_photo_b_'+br+'_c_'+co+'_p_'+po+'_'+text+'.jpg');
+                            }
+
+                            fs.unlink(preprocessedImage, function (err) {
+                                if (err){
+                                    callback(err,null)
+                                }
+                            });
+                        });
+                    }
+                });
+            }
+        }
+    }
+    res.json(200, 'started');
+ }
 
  var digits = function(req, res) {
 
@@ -104,6 +152,18 @@ function preprocessImage(path, cropRect, callback) {
         }
     });
 }
+
+function preprocessImageWithParams(path, b, c,p, callback) {
+    textcleaner.benchmark(path, b, c, p,function(err, preprocessedImage) {
+        if(err) {
+            console.error(err);
+            callback(err,null, b, c, p);
+        } else {
+            callback(null,preprocessedImage, b, c,p);
+        }
+    });
+}
+
 
 function performOCR(path, callback) {
 
